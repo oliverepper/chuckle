b29ab7cc20ffdc330d9d057abeef5f0696b5b8a3
Oliver Epper <oliver.epper@gmail.com>
2021-07-06 09:32:57 +0200

    added old and slow Intel stuff to the build

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # chuckle
 
-Very simple C++ demo library that downloads and returns a joke from [chucknorris.io](https://api.chucknorris.io/jokes/random).
-The build script builds the library for iOS, iOS simulator and macOS. It's meant to be included in a Swift package for easy
-integration into an application. The build-system links every required object file into the library.
+Very simple C++ demo library that downloads and returns a joke from
+[chucknorris.io](https://api.chucknorris.io/jokes/random).
+The build script builds the library for iOS, iOS simulator and macOS on Apple Silicon and Intel Macs.
+It's meant to be included in a Swift package for easy integration into an application.
+The build-system links every required object file into the library.
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# iOS & simulator running on arm64
+# iOS & simulator running on arm64 & x86_64
 cmake -S ./ -DCMAKE_BUILD_TYPE=RelWithDebInfo \
             -DPLATFORM=OS64 \
             -DDEPLOYMENT_TARGET=14.0 \
@@ -14,7 +14,14 @@ cmake -S ./ -DCMAKE_BUILD_TYPE=RelWithDebInfo \
             -DCMAKE_TOOLCHAIN_FILE=ios.toolchain.cmake \
             -DHAVE_SOCKET_LIBSOCKET=FALSE \
             -DHAVE_LIBSOCKET=FALSE \
-            -B out/simulatorarm64
+            -B out/simulator_arm64
+cmake -S ./ -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+            -DPLATFORM=SIMULATOR64 \
+            -DDEPLOYMENT_TARGET=14.0 \
+            -DCMAKE_TOOLCHAIN_FILE=ios.toolchain.cmake \
+            -DHAVE_SOCKET_LIBSOCKET=FALSE \
+            -DHAVE_LIBSOCKET=FALSE \
+            -B out/simulator_x86_64
 
 # macOS on arm64
 cmake -S ./ -DCMAKE_BUILD_TYPE=RelWithDebInfo \
@@ -24,16 +31,37 @@ cmake -S ./ -DCMAKE_BUILD_TYPE=RelWithDebInfo \
             -DHAVE_LIBSOCKET=FALSE \
             -B out/mac_arm64
 
+# macOS on x86_64
+cmake -S ./ -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+            -DPLATFORM=MAC \
+            -DCMAKE_TOOLCHAIN_FILE=ios.toolchain.cmake \
+            -DHAVE_SOCKET_LIBSOCKET=FALSE \
+            -DHAVE_LIBSOCKET=FALSE \
+            -B out/mac_x86_64
+
 cmake --build ./out/os64 --config RelWithDebInfo --parallel 8
-cmake --build ./out/simulatorarm64 --config RelWithDebInfo --parallel 8
+cmake --build ./out/simulator_arm64 --config RelWithDebInfo --parallel 8
+cmake --build ./out/simulator_x86_64 --config RelWithDebInfo --parallel 8
 cmake --build ./out/mac_arm64 --config RelWithDebInfo --parallel 8
+cmake --build ./out/mac_x86_64 --config RelWithDebInfo --parallel 8
 
 rm -rf libchuckle.xcframework
 
+mkdir -p "out/mac/chuckle/"
+mkdir -p "out/simulator/chuckle/"
+
+lipo -create out/mac_arm64/chuckle/libchuckle.a \
+             out/mac_x86_64/chuckle/libchuckle.a \
+     -output out/mac/chuckle/libchuckle.a
+
+lipo -create out/simulator_arm64/chuckle/libchuckle.a \
+             out/simulator_x86_64/chuckle/libchuckle.a \
+     -output out/simulator/chuckle/libchuckle.a
+
 xcodebuild -create-xcframework \
   -library "out/os64/chuckle/libchuckle.a" \
-  -library "out/simulatorarm64/chuckle/libchuckle.a" \
-  -library "out/mac_arm64/chuckle/libchuckle.a" \
+  -library "out/simulator/chuckle/libchuckle.a" \
+  -library "out/mac/chuckle/libchuckle.a" \
   -output libchuckle.xcframework
 
 # copy Header
